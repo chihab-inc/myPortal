04e53b6eeeb950833f26098c48285fb8bbb0ad37
Chihabeddine AOURINMOUCHE <chihabeddine.aourinmouche@non.se.com>
2023-09-12 18:06:42 +0200

    New DB structure

--- public/controllers/database/sectionDB.js
+++ public/controllers/database/sectionDB.js
@@ -1,60 +1,51 @@
-import { db } from './db.js'
-
 const sectionDB = {}
-const DB_NAME = 'DATA-BASE'
-const COLLECTION_NAME = 'sections'
+const DB_NAME = 'sections'
 
-db.init(DB_NAME, COLLECTION_NAME)
+const get = item => JSON.parse(localStorage.getItem(item))// TODO - MOVE TO WEB_UTILS
+const set = (item, value) => localStorage.setItem(item, JSON.stringify(value))// TODO - MOVE TO WEB_UTILS
 
-// Insert new section instance into database
+sectionDB.init = () => {
+    if (!get(DB_NAME)) {
+        set(DB_NAME, [])
+    }
+}
+
+sectionDB.update = callBack => {
+    let sections = get(DB_NAME)
+    callBack(sections)
+    set(DB_NAME, sections)
+}
+
+sectionDB.updateSectionPropertyById = (id, prop, value) => {
+    sectionDB.update(sections => {
+        sections.find(s => s.id === id)[prop] = value
+    })
+}
+
 sectionDB.createSection = data => {
-    db.updateDB(DB_NAME, dataBase => {
-        dataBase[COLLECTION_NAME].push(data)
+    sectionDB.update(sections => {
+        sections.push(data)
     })
 }
 
-// Update section instance property with new value using section id
-sectionDB.updateSectionPropertyById = data => {
-    db.updateDB(DB_NAME, dataBase => {
-        Object.keys(data).filter(k => ![null, undefined].includes(data[k])).forEach(d => {
-            dataBase[COLLECTION_NAME].find(s => s.id === data.id)[d] = data[d]
-        })
-    })
-}
+sectionDB.getSectionById = id => get(DB_NAME).find(s => s.id === id)
+sectionDB.getAllSections = () => get(DB_NAME)
+sectionDB.getSectionCount = () => sectionDB.getAllSections().length
 
-// Fetch section instance from database using section id
-sectionDB.getSectionById = id => {
-    return db.getDatabase(DB_NAME)[COLLECTION_NAME].find(s => s.id === id)
-}
+sectionDB.getTitleById = id => sectionDB.getSectionById(id).title
+sectionDB.updateTitledById = (id, title) => sectionDB.updateSectionPropertyById(id, 'title', title)
 
-// Fetch all section instances from database
-sectionDB.getAllSections = () => {
-    return db.getDatabase(DB_NAME)[COLLECTION_NAME]
-}
+sectionDB.getColorAccentById = id => sectionDB.getSectionById(id).colorAccent
+sectionDB.updateColorAccentdById = (id, colorAccent) => sectionDB.updateSectionPropertyById(id, 'colorAccent', colorAccent)
 
-// Coount all section instances in database
-sectionDB.getSectionCount = () => {
-    return sectionDB.getAllSections().length
-}
+sectionDB.getExtendedViewById = id => sectionDB.getSectionById(id).extendedView
 
-// Remove section instance from database using section id
+sectionDB.disableExtendedViewById = id => sectionDB.updateSectionPropertyById(id, 'extendedView', false)
+sectionDB.toggleExtendedViewById = id => sectionDB.updateSectionPropertyById(id, 'extendedView', sectionDB.getExtendedViewById(id))
+
 sectionDB.deleteSectionById = id => {
-    db.updateDB(DB_NAME, dataBase => {
-        dataBase[COLLECTION_NAME] = dataBase[COLLECTION_NAME].filter(s => s.id !== id)
-    })
-}
-
-// Toggle extended view of content
-sectionDB.toggleExtendedViewById = id => {
-    db.updateDB(DB_NAME, dataBase => {
-        dataBase[COLLECTION_NAME].find(s => s.id === id).extendedView = !dataBase[COLLECTION_NAME].find(s => s.id === id).extendedView
-    })
-}
-
-// Toggle extended view of content
-sectionDB.disableExtendedViewById = id => {
-    db.updateDB(DB_NAME, dataBase => {
-        dataBase[COLLECTION_NAME].find(s => s.id === id).extendedView = false
+    db.update(sections => {
+        sections = sections.filter(s => s.id !== id)
     })
 }
 
