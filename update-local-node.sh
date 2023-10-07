efdeea3595225491f1506d3ad38af69a08819de4
CHihabeddine AOURINMOUCHE <33582082+chihabeddineAourinmouche@users.noreply.github.com>
2023-10-07 16:43:19 +0200

    F/release archive scripts (#132)
    * 'Fix scripts for running node-darwin and node-linux'
    
    * Fix update scripts
    
    * Fix update scripts
    
    * Fix update scripts
    
    * Fix update scripts
    
    * Fix update scripts
    
    * 'Scripts to manage release archive'
    
    * ...
    
    ---------
    
    Co-authored-by: ChihabGoku <chihabgoku@pop-os.localdomain>

--- update-local-node.sh
+++ update-local-node.sh
@@ -4,6 +4,27 @@
 ## THIS SCRIPT WILL REPLACE OR ADD VERSIONS OF NODE BINARIES FOR BOTH LINUX, MACOS, AND WINDOWS TO THE DIRECTORY
 ## IT WILL ALSO NPM UINSTALL ANY NEWLY ADDED MODULES SO THAT THE USER DOESN'T HAVE TO DO THAT
 
+# THIS IS DEV ENVIRONMENT
+# CREATE myPortal
+mkdir myPortal && \
+# COPY public INTO myPortal
+cp -R public/ myPortal/ && \
+# REMOVE script.js FROM myPortal
+rm myPortal/public/script.js && \
+# COPY run-xxx.xx INTO myPortal
+cp run-linux.sh run-macos.sh run-windows.bat myPortal/ && \
+# COPY README.md INTO myPortal
+cp README.md myPortal/ && \
+# COPY index.js INTO myPortal
+cp index.js myPortal/ && \
+# Bundle frontend app
+npm run build && \
+
+# PREPARING MODULE INSTALL
+# check if package-lock.json exists and if so remove to it overwrite later with a more recent version
+if [ -f "package-lock.json" ]; then
+  rm package-lock.json
+fi && \
 # check if package-lock.json exists and if so remove to it overwrite later with a more recent version
 if [ -f "package-lock.json" ]; then
   rm package-lock.json
@@ -12,17 +33,11 @@ fi && \
 if [ -f "node_modules" ]; then
   rm -rf node_modules
 fi && \
-# check if package-lock.json exists and if so remove to it overwrite later with a more recent version
-if [ -f "package-lock.json" ]; then
-  rm package-lock.json
-fi && \
-# check if node_modules exists and if so remove to it overwrite later with a more recent version
-if [ -f "node_modules" ]; then
-  rm -rf node_modules
-fi && \
+npm cache clean --force && \
 echo '[ NPM INSTALL ]' && \
 npm install --save && \
 
+# PREPARING USER ENVIRONMENT
 mkdir -p node
 
 echo '[ DOWNLOADING NODE ]' && \
@@ -52,5 +67,18 @@ tar -xzf node-v20.8.0-darwin-x64.tar.gz && \
 rm node-v20.8.0-darwin-x64.tar.gz && \ 
 cp node-v20.8.0-darwin-x64/bin/node ./node/node-darwin && \ 
 rm -R node-v20.8.0-darwin-x64/ && \
+
+# COPY node_modules INTO myPortal
+cp -R node_modules myPortal && \
+# COPY node INTO myPortal
+mv node myPortal && \
+# REMOVE OLDER myPortal.zip IF EXISTS
+if [ -f "myPortal.zip" ]; then
+  rm myPortal.zip
+fi && \
+# ZIP myPortal INTO myPortal.zip
+zip -r myPortal.zip myPortal && \
+# REMOVE myPortal (directory)
+rm -R myPortal && \
 
 echo 'DONE'
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    entry: {
+      app: './public/script.js',
+    },
+    output: {
+      filename: 'script.js',
+      path: __dirname + '/myPortal/public',
+    },
+  };
